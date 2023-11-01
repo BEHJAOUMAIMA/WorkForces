7e2bfca710a792939e52207e9d2afb4a1cd477b8
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-11-01 16:20:08 +0100

    add antitymanagerfactory & transactions & mysql beans + update employee Entity + add Java persistence dependency

--- src/main/java/example/com/entities/Employee.java
+++ src/main/java/example/com/entities/Employee.java
@@ -1,9 +1,13 @@
 package example.com.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
-
+@Entity
 public class Employee {
-
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long Id;
 
     private String fullName;
